786ef019727ab5d64423a6050cb96beb53fd9308
smkrizan <smkrizan@gmail.com>
2020-10-05 14:55:07 -0500

    displaying repo name and issues to page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,81 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+// GLOBAL VARIABLES
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+// executed upon a form submission browser event
+var formSubmitHandler = function (event) {
+    // with form submission, the following expression prevents stops the browser from sending the form's input data to a URL so that input form data can instead be managed by this program
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    // send value to getUserRepos() or prompt entry
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
 };
-getUserRepos();
+
+// accepts HTTP response array of repo data and specified search term as parameters
+var displayRepos = function(repos, searchTerm) {
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loops over repos
+    for (var i = 0; i < repos.length; i++) {
+        // formats repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+      
+        // creates container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+      
+        // creates span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+      
+        // appends span element to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check whether current repo has open issues
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+    
+        // append status check to container
+        repoEl.appendChild(statusEl);    
+      
+        // appends container to the dom
+        repoContainerEl.appendChild(repoEl);
+    };
+};
+
+// dynamic HTTP request to server
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        // callback to send to display function
+        response.json().then(function(data) {
+            displayRepos(data, user);
+        });
+    });
+};
+
+// event listener for userFormEl
+userFormEl.addEventListener("submit", formSubmitHandler);
